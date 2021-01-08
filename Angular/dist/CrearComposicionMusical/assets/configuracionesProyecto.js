7b00eca45fc756562eb7581fa35583c4939670c1
None None <joselizarraga.m@gmail.com>
2021-01-08 18:30:02 -0300

    cambios

--- Angular/dist/CrearComposicionMusical/assets/configuracionesProyecto.js
+++ Angular/dist/CrearComposicionMusical/assets/configuracionesProyecto.js
@@ -1,4 +1,4 @@
-
 const configuracionesProyecto = {
-    rutaWebApi: "http://localhost:8080/api"
+    //rutaWebApi: "http://localhost:8080/api"
+	rutaWebApi: "https://crear-composicion-musical.herokuapp.com/api"
 }
